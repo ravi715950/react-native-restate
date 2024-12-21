fd4520904eb3bca7c75666609265159641c4f4fa
ravi715950 <ravi715950@gmail.com>
2024-12-22 02:54:19 +0530

    code commited for add font and routing

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-3xl font-bold font-rubik mb-10 text-danger">
+        Welcome to Restate
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/4">Property</Link>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const SignIn=()=>{
+    return(
+        <View>
+            <Text>Hello Sign In</Text>
+        </View>
+    )
+}
+
+export default SignIn;
