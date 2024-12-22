77f5376b857471eb3fba22edb2ce05e29cf50b6a
ravi715950 <ravi715950@gmail.com>
2024-12-22 13:28:02 +0530

    code commited for add tabs

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import { useGlobalContext } from "@/lib/global-provider";
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -16,10 +15,6 @@ export default function Index() {
       <Text className="text-3xl font-bold font-rubik mb-10 text-danger">
         Welcome {user?.name || "User"}
       </Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/4">Property</Link>
     </View>
   );
 }
